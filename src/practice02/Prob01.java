967a843477dc42842cc512509312029c39d92ff1
choi <choi@192.168.0.70>
2018-12-26 17:46:45 +0900

    test

--- src/practice02/Prob01.java
+++ src/practice02/Prob01.java
@@ -4,6 +4,7 @@ public class Prob01 {
 
 	public static void main(String[] args) {
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+		System.out.println("dfdf");
 	}
 
 }
